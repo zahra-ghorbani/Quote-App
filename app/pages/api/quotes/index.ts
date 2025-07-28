b57fcce6c921b63ffce389e3823069b0279b59b8
unknown <vieraviera7718@gmail.com>
2025-07-28 12:14:16 +0330

    add pagination

--- app/pages/api/quotes/index.ts
+++ app/pages/api/quotes/index.ts
@@ -1,34 +1,37 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { Quote } from "@/app/types/quote";
+import { allQuotes } from "@/app/lib/quote";
+
+const PAGEـSIZE = 10;
 
 
-let quotes: Quote[] = [
-    {
-        id: '1',
-        text: 'salaaaaaaaaam',
-        author: 'me',
-        createdAt: new Date().toDateString()
-    }
-];
+
+
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'GET') {
-        return res.status(200).json(quotes);
-
-    }
+        const page = parseInt(req.query.page as string) || 1;
+        const start = (page - 1) * PAGEـSIZE;
+        const end = start + PAGEـSIZE;
+        const paginated = allQuotes.slice(start, end);
+        return res.status(200).json(paginated);
+  
+    } 
     if (req.method === 'POST') {
-        const{text, author}=req.body;
-       if(!text || !author)return res.status(400).json({message:'the information is incomplete'});
+        const { text, author } = req.body;
+        if (!text || !author) return res.status(400).json({ message: 'the information is incomplete' });
 
-       const newQuote :Quote={
-        id:Date.now().toString(),
-        text,
-        author,
-        createdAt:new Date().toISOString()
+        const newQuote: Quote = {
+            id: Date.now().toString(),
+            text,
+            author,
+            createdAt: new Date().toISOString()
 
-       };
-       quotes.push(newQuote);
-       return res.status(201).json(newQuote);
+        };
+
+        allQuotes.unshift(newQuote);
+
+        return res.status(201).json(newQuote);
     }
-    res.setHeader('ALLOW',['GET','POST']);
+    res.setHeader('ALLOW', ['GET', 'POST']);
     res.status(405).end(`metod${req.method} Not Allowed`)
 }
